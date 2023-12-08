2cc36ee2ef77ab6daae42d2b2d4a0efc9d4b6066
dobatruong <batruongdo3@gmail.com>
2023-12-08 11:30:58 +0700

    update

--- src/components/Elements/DataDisplay/MyTreeItem.tsx
+++ src/components/Elements/DataDisplay/MyTreeItem.tsx
@@ -5,6 +5,15 @@ const StyledTreeItem = styled(TreeItem)`
     & > .MuiTreeItem-content {
         height: 25px;
     }
+    & > .MuiTreeItem-content.Mui-selected, 
+    .MuiTreeItem-content.Mui-selected.Mui-focused, 
+    .MuiTreeItem-content.Mui-focused {
+        background-color: #c8e3de;
+        cursor: pointer;
+    }
+    &:hover {
+        background-color: #c8e3de;
+    }
 `;
 
 export function MyTreeItem(props: TreeItemProps) {
