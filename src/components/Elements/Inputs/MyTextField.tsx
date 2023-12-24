b023d9a4dad9a275eb5b8be639f3a163e7b6fc85
dobatruong <batruongdo3@gmail.com>
2023-12-24 18:18:07 +0700

    update

--- src/components/Elements/Inputs/MyTextField.tsx
+++ src/components/Elements/Inputs/MyTextField.tsx
@@ -4,8 +4,17 @@ import { forwardRef } from 'react';
 const StyledTextField = styled(TextField)`
     width: 350px;
     height: 42px;
+    font-size: 14px;
+    & label.Mui-focused {
+        color: #0e8a72;
+    }
+    & .MuiOutlinedInput-root {
+        &.Mui-focused fieldset {
+            border-color: #0e8a72;
+        }
+    }
 `;
 
 export const MyTextField = forwardRef<HTMLDivElement, TextFieldProps>((props, ref) => {
-    return <StyledTextField ref={ref} {...props}/>;
+    return <StyledTextField ref={ref} {...props} />;
 });
