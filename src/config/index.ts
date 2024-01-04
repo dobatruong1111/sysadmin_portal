7936a79e4e16bdfa90f63be458e997e36d59f1d8
Nguyễn Hồng Vân <hongvan.bbx.ozanni@gmail.com>
2024-01-04 11:14:52 +0700

    add .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-export const DEFAULT_USERNAME = "admin";
-export const DEFAULT_PASSWORD = "72131@admin";
+export const DEFAULT_USERNAME = import.meta.env.VITE_DEFAULT_USERNAME as string;
+export const DEFAULT_PASSWORD = import.meta.env.VITE_DEFAULT_PASSWORD as string;
 export const TECHNICAL_SUPPORT = "0962.800.362";
 
 // API endpoints
-export const API_URL = "http://27.72.147.196:47878/sysadmin/ws/rest/v1";
-export const API_URL_LOGIN = 'http://27.72.147.196:39999/ws/rest/v1';
-export const API_URL_QA = "http://27.72.147.196:39999/sysadmin/ws/rest/v1";
+export const API_URL = import.meta.env.VITE_API_URL as string;
+export const API_URL_LOGIN = import.meta.env.VITE_API_URL_LOGIN as string;
+export const API_URL_QA = import.meta.env.VITE_API_URL_QA as string;
 
